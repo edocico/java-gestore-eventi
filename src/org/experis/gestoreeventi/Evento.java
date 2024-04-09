bfedf0b259bd1b3a1914d3a2733476c9b1f83f42
edocico <138378767+edocico@users.noreply.github.com>
2024-04-09 09:40:53 +0200

    add evento constructor getter and setter

--- src/org/experis/gestoreeventi/Evento.java
+++ src/org/experis/gestoreeventi/Evento.java
@@ -1,4 +1,60 @@
 package org.experis.gestoreeventi;
 
+import java.time.LocalDate;
+
 public class Evento {
+
+    // attributi
+
+    private String titolo;
+    private LocalDate data;
+    private int postiTotali;
+    private int postiPrenotati;
+
+    // costruttore
+
+
+    public Evento(String titolo, LocalDate data, int postiTotali) throws IllegalArgumentException{
+
+        if (data.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("La data dell'evento non può essere passata");
+        }
+        if (postiTotali <= 0) {
+            throw new IllegalArgumentException("Il numero di posti totali deve essere positivo");
+        }
+        this.titolo = titolo;
+        this.data = data;
+        this.postiTotali = postiTotali;
+        this.postiPrenotati = 0;
+    }
+
+    // setter
+
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public void setPostiTotali(int postiTotali) {
+        this.postiTotali = postiTotali;
+    }
+
+    public void setPostiPrenotati(int postiPrenotati) {
+        this.postiPrenotati = postiPrenotati;
+    }
+
+    // getter
+
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
 }
